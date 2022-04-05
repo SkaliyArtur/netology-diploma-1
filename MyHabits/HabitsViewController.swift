e69b7ec7c6b43831a744e5cf40c7ef63beb16343
SkaliyArtur <skaliy.art@gmail.com>
2022-04-06 00:15:40 +0300

    Сделал navigationBarЫ и кнопку добавить, сделал таббар

--- MyHabits/HabitsViewController.swift
+++ MyHabits/HabitsViewController.swift
@@ -7,23 +7,36 @@
 
 import UIKit
 
-class HabitsViewController: UIViewController {
+class HabitsViewController: UIViewController, UINavigationBarDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    
+    func setupNavBar () {
+        
+        let navigationBar: UINavigationBar = {
+            let navBar = UINavigationBar()
+            navBar.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 140)
+            navBar.backgroundColor = UIColor(red: 249/255.0, green: 249/255.0, blue: 249/255.0, alpha: 0.94)
+            return navBar
+        }()
+        
+        navigationBar.delegate = self
+        view.addSubview(navigationBar)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(tap))
+        navigationItem.rightBarButtonItem!.tintColor = UIColor(red: 161/255.0, green: 22/255.0, blue: 204/255.0, alpha: 1.0)
+        navigationItem.title = "Сегодня"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(red: 242/255.0, green: 242/255.0, blue: 242/255.0, alpha: 0.94)
+        
+        setupNavBar()
     }
-    */
-
+    @objc func tap() {
+//        let infoVC = InfoViewController()
+//        self.present(infoVC, animated: true, completion: nil)
+        print("Hello")
+    }
 }
